fb5590eb26e5447dfd3114efc336cccf2b02866f
minhbear <minhbeardev@gmail.com>
2024-06-19 00:41:19 +0700

    feat: init lambda serverless base

---
+++ functions/connect_bedrock.py
@@ -0,0 +1,6 @@
+def connect_bedrock(event, context):
+  print("The first function has been invoked!!")
+  return {
+    'statusCode': 200,
+    'body': "Hello, World!.\n This is the first function."
+  }
---
+++ handler.py
@@ -0,0 +1,1 @@
+from functions.connect_bedrock import connect_bedrock
